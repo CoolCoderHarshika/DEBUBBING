524cb01f22bdda91162031b9a12a1fea73f6b59e
CoolCoderHarshika <harshikabajwa@gmail.com>
2020-10-27 17:06:30 +0530

    Debug

--- Pig.js
+++ Pig.js
@@ -3,6 +3,7 @@ class Pig extends BaseClass {
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
     this.Visiblity = 255;
+    //this.score=0
   }
 
  display(){
@@ -20,7 +21,13 @@ class Pig extends BaseClass {
    }
    
  }
-
+ score1()
+ {
+   if(this.Visiblity<0 && this.Visiblity>-1000)
+   {
+      score=score+1
+   }
+ }
 
 
 };
